6a57b737bb96ed6b1edbb9e47fb1de922c6cb891
Sven Rubanschi <sven_rubi@hotmail.de>
2019-05-07 13:33:17 +0200

    uebung

---
+++ Uebung/blatt02b.py
@@ -0,0 +1,5 @@
+for i in range(1,21):
+	for j in range(1,21):
+		erg = i*j
+		print("   " + str(erg), end = '')
+	print("\n")
---
+++ Uebung/blatt02c.py
@@ -0,0 +1,10 @@
+kapital = float(input("Enter your capital "))
+zielKapital = float(input("Enter your target capital "))
+ZINS = float(input("The interest rate "))
+jahr = 0
+
+while kapital<zielKapital:
+	kapital = kapital + (kapital * (ZINS/100))
+	jahr = jahr + 1
+
+print("It takes %d years" % (jahr))
